0769931a1fb74f5c0d81acd07fe98f412bfe5b5b
hrl4887a <allan.hoarau@etu.iut-tlse3.fr>
2025-09-29 11:06:11 +0200

    modification bonjour

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,8 +10,8 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "Allan", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+    @GetMapping("/bonjour")
+    public String hello() {
+      return "Bonjour le monde !";
     }
 }
